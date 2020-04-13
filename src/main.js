ee5f5c2b9e4d5780e842f27d6d32b92e69bd525e
molifei <574579565@qq.com>
2020-04-13 14:47:30 +0800

    reset css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 初始化css
+// pc
+// import "@/assets/css/pc/base.css"
+// 移动
+// import "@/assets/css/mb/base.css"
+
+
 // 按需加载element-UI
 import {Button,Message} from "element-ui"
 Vue.use(Button)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="home">
         <el-button type="primary">主要按钮</el-button>
+        <p>{{ msg }}</p>
     </div>
 </template>
 
 <script>
-
     export default {
         name: "Home",
         data() {
-            return {}
-        },
-
+            return {
+                msg:"你好"
+            }
+        }
     }
 </script>
 
